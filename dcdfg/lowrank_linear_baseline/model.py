b487a78b631993076212575dcf89ce3e6bd84a35
Eric Kernfeld <eric.kern13@gmail.com>
2022-12-14 17:01:19 -0500

    Add simulateKO method

--- dcdfg/lowrank_linear_baseline/model.py
+++ dcdfg/lowrank_linear_baseline/model.py
@@ -207,3 +207,19 @@ class LinearModuleGaussianModel(pl.LightningModule):
         # if we found a stationary point, that satisfies the acyclicity constraints, raise this flag, it will activate patience and terminate training soon
         else:
             self.trainer.satisfied_constraints = True
+            
+    def simulateKO(self, control_expression: np.ndarray, KO_gene_idx: int, KO_gene_value: float = 0, maxiter=100, maxiter_cyclic = 1):
+        """Simulate a knockout experiment outcome given control expression and given which gene is KO'd."""
+        if not self.module.check_acyclicity():
+            print(f"Warning: graph is not acyclic. Predictions may diverge (give NaN's). Setting maxiter to {maxiter_cyclic}.")
+            maxiter = maxiter_cyclic
+        x = torch.from_numpy(control_expression)
+        x = x.float()
+        for i in range(maxiter):
+            xold = x
+            x[:, KO_gene_idx] = KO_gene_value
+            x = self.module.forward(x)
+            x[:, KO_gene_idx] = KO_gene_value
+            if torch.linalg.vector_norm(xold - x) < 1e-12:
+                break
+        return x.detach().numpy()
